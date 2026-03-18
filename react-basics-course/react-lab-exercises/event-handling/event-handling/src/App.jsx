db2a5753555ceaee3a233efa494ca330110f8303
Allen <132642677+Allen1303@users.noreply.github.com>
2026-03-17 20:24:50 -0500

    Added Event handling Exercise with use State

---
+++ react-basics-course/react-lab-exercises/event-handling/event-handling/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [result, setResult] = useState(null);
+  const { randomNum, userInput } = result || {};
+  const buttonStyle = {
+    backgroundColor: "steelblue",
+    color: "whitesmoke",
+    padding: "10px 20px",
+    width: "50%",
+    borderRadius: "0.5rem",
+    cursor: "pointer",
+  };
+  //
+  // const containerStyle = {
+  //   display: "flex",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  // };
+  const handleClick = () => {
+    let randomNum = Math.floor(Math.random() * 5) + 1;
+    // console.log(randomNum);
+    let userInput = prompt("Take a guess");
+    setResult({ randomNum, userInput });
+  };
+  return (
+    <>
+      <div>
+        <h1>Event Handling With UseState</h1>
+        <button onClick={handleClick} style={buttonStyle}>
+          Guess a Number Between 1 And 5
+        </button>
+        {result && (
+          <p>
+            Computer: <srpong>{randomNum}</srpong>, Your Guess:{" "}
+            <strong>{userInput}</strong>
+          </p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
