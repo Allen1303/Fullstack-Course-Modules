ed14bfb03129c4f3f9e233b89d95456f11ded955
Allen <132642677+Allen1303@users.noreply.github.com>
2026-04-13 09:28:42 -0500

    Refactor Article component + add input toggle switch to header component

---
+++ advance-react/react-lab-exercises/react-context-exercise/src/components/ToggleSwitch.jsx
@@ -0,0 +1,10 @@
+// Toggle Switch Control Light Adn Dark Theme
+function ToggleSwtich() {
+  return (
+    <label>
+      <input type="checkbox" />
+      <span></span>
+    </label>
+  );
+}
+export default ToggleSwtich;
